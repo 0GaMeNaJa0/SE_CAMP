468f16db9bec0ac377a1831ac50b59b2d264d56e
0GaMeNaJa0 <herogane13748@gmail.com>
2023-12-18 19:46:48 +0700

    php commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <?php 
+
+        ?>
+        <h1>Hello world <?php echo "Hell world"; ?></h1>
+        <div>
+            <p>1</p>
+            <?php
+            {       
+                echo "<h1>my h1</h1>"      
+            ?>
+            <p>2</p>
+            <?php
+            }
+            ?>
+            <p>3</p>
+        </div>
+    </body>
+</html>
