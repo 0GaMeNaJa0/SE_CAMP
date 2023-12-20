9b5da78979e193e5c640a4c8b6ce9445a7b1b335
0GaMeNaJa0 <herogane13748@gmail.com>
2023-12-20 16:56:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
 <html>
-    <body>
-        <?php 
 
-        ?>
-        <h1>Hello world <?php echo "Hell world"; ?></h1>
-        <div>
-            <p>1</p>
-            <?php
-            {       
-                echo "<h1>my h1</h1>"      
-            ?>
-            <p>2</p>
-            <?php
-            }
-            ?>
-            <p>3</p>
-        </div>
-    </body>
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            <tr>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
+</body>
+
 </html>
