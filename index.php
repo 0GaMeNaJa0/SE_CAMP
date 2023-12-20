6f3baa25e3e285f54907814f8a4dfbc5be66bd3c
0GaMeNaJa0 <herogane13748@gmail.com>
2023-12-20 17:50:41 +0700

    multiplication table

--- index.php
+++ index.php
@@ -2,29 +2,31 @@
 
 <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    <?php $multi_x = 3; ?>
+    <div>
+        <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
 
-    <table>
-        <tbody>
-            <tr>
-                <td></td>
-                <td>x</td>
-                <td></td>
-                <td>=</td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
-    <style>
-        table {
-            width: 150px;
-            height: 265px;
-        }
-    </style>
+        <table>
+            <tbody>
+                <?php
+                for ($i = 1; $i <= 24; $i++) {
+                    $multiple_value = $i * $multi_x;
+                    echo "<tr>";
+                    echo "<td>$multi_x</td>";
+                    echo "<td>x</td>";
+                    echo "<td>$i</td>";
+                    echo "<td>=</td>";
+                    echo "<td>$multiple_value</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
