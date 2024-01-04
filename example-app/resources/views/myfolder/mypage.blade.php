9b44b91a8e0adb1f84fa82cc24d0cb3e0ab2931d
0GaMeNaJa0 <herogane13748@gmail.com>
2024-01-04 19:54:57 +0700

    laravel wk2 controller

--- example-app/resources/views/myfolder/mypage.blade.php
+++ example-app/resources/views/myfolder/mypage.blade.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>My Folder and My Page</h1>
-    <h1>
-        <form method="post" action = "/my-route">
-            @csrf
-            <input type="text" name ="myinput">
-            <button type = "submit">Submit</button>
-        </form>
-    </h1>
-</body>
-</html>
+<h1>My Folder and My Page</h1>
+<h1>
+    <?php echo $val_a . ' ' . $val_b; ?>
+</h1>
+<h1>{{ $val_a . ' ' . $val_b }}</h1>
+<form method="post" action="/my-controller4">
+    @csrf
+    <input type="text" name="myinput">
+    <button type="submit">Submit</button>
+</form>
